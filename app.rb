066eaafda9f810b307cc220d975d78ea6db4fbf5
Adriana <adriana.amaral@msn.com>
2021-03-18 10:13:22 -0500

    commit tree 31c071c971b4c8c5662e846c1e3bf4665a1b1091

--- app.rb
+++ app.rb
@@ -5,5 +5,34 @@ module FormsLab
 
     # code other routes/actions here
 
-  end
+    get '/' do 
+
+      erb :root
+    end 
+
+    get '/new' do 
+
+      erb :'pirates/new'
+    end 
+
+    get 'pirates/new' do 
+
+      erb :'pirates/new'
+    end 
+
+    post "/pirates" do
+
+      @pirate = Pirate.create(name: params['name, weight, height'])
+      # redirect "/pirates/#{@pirate.id}"
+
+      # @pirate = Pirate.create(params['name, weight, height'])
+      # params[:pirate][:ships].each do |details|
+      #   Ship.new(details)
+      # end
+      # @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
+  end 
 end
